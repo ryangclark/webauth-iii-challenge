924d78fcccb8974a15d32dad07f581887bcf7a52
Clark <rclark010@gmail.com>
2019-04-03 14:10:02 -0600

    Added bouncer midleware to users routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+cors = require('cors');
 express = require('express');
 helmet = require('helmet');
 
+bouncer = require('../auth/bouncer').default;
+
+authRoutes = require('./auth/auth-routes').default;
+userRoutes = require('./users/users-routes');
+
 server = express();
 
-// Middleware
-server.use(express.json());
-server.use(helmet());
+// Universal Middleware
+server.use(cors(), express.json(), helmet());
 
 // Routing
+server.use('/api/encantado', authRoutes);
+server.use('/api/users', bouncer, userRoutes);
 
 export default server;
